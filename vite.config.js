6b02738794730189c71a8b1428f029073e87a5f3
Joshua <joxuecont@gmail.com>
2024-03-03 20:25:47 -0400

    add gh-pages integration CI ♻

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://joshuaco.github.io/patient-administrator-react/",
   plugins: [react()],
-})
+});
