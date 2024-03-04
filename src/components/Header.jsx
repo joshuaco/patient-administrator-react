4adf5c920684f4a65fa1f8b4a113c9a386c88137
Joshua <joxuecont@gmail.com>
2024-03-03 20:42:54 -0400

    fix icon path url

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ function Header() {
   return (
     <header className="pt-3">
       <div className="flex items-center">
-        <img src="/dog_face.svg" alt="Dog Face" className="w-14" />
-        <h1 className="text-3xl font-bold">PetCare</h1>
+        <img src="./dog_face.svg" alt="Dog Face" className="w-14" />
+        <h1 className="tehxt-3xl font-bold">PetCare</h1>
       </div>
     </header>
   );
